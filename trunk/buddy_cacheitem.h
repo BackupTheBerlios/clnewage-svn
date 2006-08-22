924fda67ffec000e6ccd955754b4d9a86d00f941
thedoctor <thedoctor@355f5bcf-a01b-0410-bed8-a39706a05ca7>
2006-08-22 21:16:40 +0000

    Added items remotely
    C:\Program Files\Microsoft Visual Studio 8\Projects\Miranda SVN [0.6]\plugins\Clist_NewAge\buddy_cacheitem.cpp
    C:\Program Files\Microsoft Visual Studio 8\Projects\Miranda SVN [0.6]\plugins\Clist_NewAge\buddy_cacheitem.h
    C:\Program Files\Microsoft Visual Studio 8\Projects\Miranda SVN [0.6]\plugins\Clist_NewAge\buddy_cacheItemList.cpp
    C:\Program Files\Microsoft Visual Studio 8\Projects\Miranda SVN [0.6]\plugins\Clist_NewAge\buddy_cacheItemList.h
    C:\Program Files\Microsoft Visual Studio 8\Projects\Miranda SVN [0.6]\plugins\Clist_NewAge\buddy_common.h
    C:\Program Files\Microsoft Visual Studio 8\Projects\Miranda SVN [0.6]\plugins\Clist_NewAge\buddy_init.cpp
    C:\Program Files\Microsoft Visual Studio 8\Projects\Miranda SVN [0.6]\plugins\Clist_NewAge\buddy_services.cpp

---
+++ trunk/buddy_cacheitem.h
@@ -0,0 +1,62 @@
+/*
+Miranda IM: the free IM client for Microsoft* Windows*
+
+Copyright 2000-2006 Miranda ICQ/IM project, 
+all portions of this codebase are copyrighted to the people 
+listed in contributors.txt.
+
+This program is free software; you can redistribute it and/or
+modify it under the terms of the GNU General Public License
+as published by the Free Software Foundation; either version 2
+of the License, or (at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+*/
+
+/*
+	File contains declaration of class implements the item of cache.
+
+    Created by Artem Shpynov mailto:ashpynov@gmail.com August 21,2006
+*/
+
+/*
+    ------------------------------------------------------------------------
+			                      FILE HISTORY
+	------------------------------------------------------------------------
+	Author				DATE		Comment
+	------------------------------------------------------------------------
+	A. Shpynov (FYR)	08/21/06	Initial Release
+
+*/
+#pragma once
+#include "buddy_common.h"
+typedef unsigned int HCITEM;
+
+class cacheItem
+{
+//C'tor & D'tor
+public:
+	cacheItem();
+	cacheItem(const cacheItem *from);
+	~cacheItem();
+
+//Internal functions
+public:
+	void init();
+	void copy(const cacheItem *fromItem);		// copy all fields of fromItem to this item.
+
+//member variables
+public:
+	HCITEM			m_hItem;				// unique indentifier of item
+	TCHAR			*m_lpName;				// buddy display name (should be copied)
+	const TCHAR		*m_lpProtocol;			// buddy protocol (pointer to constant)
+	int				m_nStatus;				// current status of contact
+};
+
